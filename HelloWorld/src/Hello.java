f55a150584c74ea12320a4410c445b1d7373e203
Caylem <caylemh@gmail.com>
2020-05-18 13:24:27 +0200

    First Steps - Variables & Expressions

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,17 @@
 public class Hello {
     public static void main(String[] args) {
+
+        // Printing a message to the console.
         System.out.println("Hello Caylem, Welcome to Java!");
+
+        //Defining variables
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+
+        System.out.println(myFirstNumber + " + " + mySecondNumber + " + " + myThirdNumber + " = " + myTotal);
+        System.out.println("1000 minus " + myTotal + " = " + myLastOne);
     }
 }
