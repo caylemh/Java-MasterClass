321a7c41a9c75de25ba6a0e345707a76a1b6e5cc
Caylem <caylemh@gmail.com>
2020-05-18 13:52:49 +0200

    First Steps - More Primitive Types

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -20,5 +20,21 @@ public class Main {
         // int myMaxIntTest = 2_147_483_648;        Value is too large
         int myMaxIntTest = 2_147_483_647;
         System.out.println(myMaxIntTest);
+
+        // Byte, Short, Long and Width
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value: " + myMinLongValue);
+        System.out.println("Long Maximum Value: " + myMaxLongValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value: " + myMinShortValue);
+        System.out.println("Short Maximum Value: " + myMaxShortValue);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value: " + myMinByteValue);
+        System.out.println("Byte Maximum Value: " + myMaxByteValue);
     }
 }
