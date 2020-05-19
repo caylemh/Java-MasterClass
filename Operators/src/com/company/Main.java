18e4ce1c8bba6658a108b7f15d1005f0df29ec3e
Caylem <caylemh@gmail.com>
2020-05-19 03:32:26 +0200

    First Steps - Operator Precedence & Challenge

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -23,5 +23,53 @@ public class Main {
 
         // Operator abbreviations:
         // result++, result--, result +=, result -=, result *=, etc.
+
+        // Conditional Statements - if-then
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an Alien!");
+        }
+
+        int topscore = 80;
+        if (topscore < 100) {
+            System.out.println("You've got the highest score!");
+        }
+
+        // Logical AND operator
+        int secondTopscore = 95;
+        if ((topscore > secondTopscore) && (topscore < 100)) {
+            System.out.println("Greater than second top score and less than 100.");
+        }
+
+        // Logical OR operator
+        if ((topscore > 90) || (secondTopscore <= 90)) {
+            System.out.println("Either or both of the conditions are true.");
+        }
+
+        boolean isCar = false;
+        if(isCar) {
+            System.out.println("This is not supposed to happen.");
+        }
+
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
+        // Operator Challenge
+        double myVal1 = 20.00d;
+        double myVal2 = 80.00d;
+
+        double myResult = (myVal1 + myVal2) * 100.00d;
+        System.out.println("myResult = " + myResult);
+        double remainder = myResult % 40.00d;
+        System.out.println("Remainder = " + remainder);
+        boolean isNoRemainder = remainder == 0 ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder!");
+        }
     }
 }
