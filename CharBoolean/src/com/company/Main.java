53400ed7f1c93847bdbd8b1adca07e9742a0aaaf
Caylem <caylemh@gmail.com>
2020-05-19 02:08:40 +0200

    First Steps - Char and Boolean

---
+++ CharBoolean/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicode = '\u0044';
+        char myCopywrite = '\u00A9';
+
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+        System.out.println(myCopywrite);
+
+        boolean myTrueBoolValue = true;
+        boolean myFalseBoolValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
