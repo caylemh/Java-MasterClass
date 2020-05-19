149bdb2c7d66821e93bc0f09c929fd8be2ce8a90
Caylem <caylemh@gmail.com>
2020-05-19 19:30:18 +0200

    Section 4: Statements, Whitespace & Code Blocks.

---
+++ StatementsWhitespaceIndexing/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Statements end with a ';'
+	    int myVariable = 50;
+	    myVariable ++;
+	    myVariable --;
+        System.out.println(myVariable);
+    }
+}
