1454e924e8f0b1e5f2b2bcf844b243e95da17129
Caylem <caylemh@gmail.com>
2020-05-20 16:28:39 +0200

    Section 4: Coding Exercise 3 - Barking Dog

--- BarkingDog/src/BarkingDog.java
+++ BarkingDog/src/BarkingDog.java
@@ -3,14 +3,11 @@ public class BarkingDog {
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
         if (barking) {
             if (((hourOfDay < 8) && !(hourOfDay < 0)) || ((hourOfDay > 22) && !(hourOfDay > 23))) {
-                System.out.println("Wake up! The dog is barking!");
                 return true;
             } else {
-                System.out.println("The dog is barking... But it's ok.");
                 return false;
             }
         }
-        System.out.println("The dog is quiet...Zzzz.");
         return false;
     }
 }
