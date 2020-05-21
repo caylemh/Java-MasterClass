cfbd3e552024f630257bb057ad68027ba8f01087
Caylem <caylemh@gmail.com>
2020-05-21 22:52:30 +0200

    Section 4: Coding Exercise - Equality Printer

---
+++ IntEqualityPrinter/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        IntEqualityPrinter.printEqual(-1,-1,1);
+        IntEqualityPrinter.printEqual(1,1,1);
+        IntEqualityPrinter.printEqual(1,2,1);
+        IntEqualityPrinter.printEqual(3,1,1);
+        IntEqualityPrinter.printEqual(1,1,5);
+        IntEqualityPrinter.printEqual(1,3,9);
+    }
+}
