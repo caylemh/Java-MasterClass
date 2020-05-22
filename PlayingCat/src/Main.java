e120910ba7cb4a6777d9e2986a2fc04dc1fa48bc
Caylem <caylemh@gmail.com>
2020-05-22 11:35:10 +0200

    Section 4: Coding Exercise - Playing Cat

---
+++ PlayingCat/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(PlayingCat.isCatPlaying(true,5));
+        System.out.println(PlayingCat.isCatPlaying(true,28));
+        System.out.println(PlayingCat.isCatPlaying(false,35));
+        System.out.println(PlayingCat.isCatPlaying(false,40));
+    }
+}
