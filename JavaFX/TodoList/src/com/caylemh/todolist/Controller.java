595dd9ba5237fc7902429dcb8d833b2662d1621e
Caylem <caylemh@gmail.com>
2020-06-11 12:30:02 +0200

    Section 13: Add & Show Dialog. Add Controller for Dialog.

--- JavaFX/TodoList/src/com/caylemh/todolist/Controller.java
+++ JavaFX/TodoList/src/com/caylemh/todolist/Controller.java
@@ -5,16 +5,18 @@ import com.caylemh.todolist.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Controller {
     private List<TodoItem> todoItems;
@@ -25,28 +27,11 @@ public class Controller {
     private TextArea itemDetailsTextArea;
     @FXML
     private Label deadlineLabel;
+    @FXML
+    private BorderPane mainBorderPane;
 
 
     public void initialize() {
-//        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
-//                LocalDate.of(2020, Month.APRIL, 22));
-//        TodoItem item2 = new TodoItem("Doctors Appointment", "See Dr. Smith at 123 Main Str. Bring paperwork",
-//                LocalDate.of(2020, Month.MAY, 5));
-//        TodoItem item3 = new TodoItem("Finish Design Proposal", "Promise Mike I'd email the mockups.",
-//                LocalDate.of(2020, Month.APRIL, 6));
-//        TodoItem item4 = new TodoItem("Pickup Doug at train station", "Doug is arriving on March 23 on the 5 o' clock train.",
-//                LocalDate.of(2020, Month.MARCH, 22));
-//        TodoItem item5 = new TodoItem("Pickup Dry Cleaning", "Clothes should be ready by Wednesday.",
-//                LocalDate.of(2020, Month.MAY, 17));
-
-//        todoItems = new ArrayList<TodoItem>();
-//        todoItems.add(item1);
-//        todoItems.add(item2);
-//        todoItems.add(item3);
-//        todoItems.add(item4);
-//        todoItems.add(item5);
-
-//        TodoData.getInstance().setTodoItems(todoItems);
 
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
@@ -66,15 +51,36 @@ public class Controller {
     }
 
     @FXML
+    public void showNewItemDialog() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("todoItemDialog.fxml"));
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            System.out.println("Could'nt load the dialog!");
+            e.printStackTrace();;
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK) {
+            DialogController controller = fxmlLoader.getController();
+            controller.processResults();
+            System.out.println("OK Pressed!");
+        } else {
+            System.out.println("Cancelled Pressed!");
+        }
+    }
+
+    @FXML
     public void handleClickListView() {
         TodoItem item = todoListView.getSelectionModel().getSelectedItem();
         itemDetailsTextArea.setText(item.getDetails());
         deadlineLabel.setText(item.getDeadline().toString());
-        //System.out.println("Selected item is " + item);
-//        StringBuilder sb = new StringBuilder(item.getDetails());
-//        sb.append("\n\n\n\n");
-//        sb.append("Due: ");
-//        sb.append(item.getDeadline().toString());
-//        itemDetailsTextArea.setText(sb.toString());
     }
 }
