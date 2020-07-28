9d302bd37f67856a81b1f077da01325ac651b17b
Caylem <caylemh@gmail.com>
2020-07-28 14:43:25 +0200

    Section 8: Banking App Challenge

---
+++ BankApp/src/Bank.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    public boolean addBranch(String branchName) {
+        if(findBranch(branchName) == null) {
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomer(String branchName, String customerName, double initialAmount) {
+        Branch branch = findBranch(branchName);
+        if(branch != null) {
+            return branch.newCustomer(customerName, initialAmount);
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String branchName, String customerName, double amount) {
+        Branch branch = findBranch(branchName);
+        if(branch != null) {
+            return branch.addCustTransaction(customerName,amount);
+        }
+        return false;
+    }
+
+    private Branch findBranch(String branchName) {
+        for(Branch checkedBranch: this.branches) {
+            if(checkedBranch.getName().equals(branchName)) {
+                return checkedBranch;
+            }
+        }
+        return null;
+    }
+
+    public boolean listCustomers(String branchName, boolean showTransactions) {
+        Branch branch = findBranch(branchName);
+        if(branch != null) {
+            System.out.println("\nCustomer details for branch: " + branch.getName() + "\n");
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+            for(Customer branchCustomer: branchCustomers) {
+                System.out.println("\tCustomer: " + branchCustomer.getName() + "[" +
+                        ((branchCustomers.indexOf(branchCustomer))+1) + "]");
+                if(showTransactions) {
+                    System.out.println("Transactions");
+                    ArrayList<Double> transactions = branchCustomer.getTransactions();
+                    for(int i=0; i<transactions.size(); i++) {
+                        System.out.println("\t[" + (i+1) + "] Amount --> " + transactions.get(i));
+                    }
+                    System.out.print("\n");
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+}
---
+++ BankApp/src/Main06.java
@@ -0,0 +1,33 @@
+public class Main06 {
+    public static void main(String[] args) {
+        Bank bank = new Bank("Standard Bank");
+        bank.addBranch("Melville");
+        bank.addCustomer("Melville", "Caylem",10.00);
+        bank.addCustomer("Melville", "Mike",100.00);
+        bank.addCustomer("Melville", "James",180.00);
+
+        bank.addBranch("JHB");
+        bank.addCustomer("JHB", "John",20.00);
+        bank.addCustomer("JHB", "Louis",260.00);
+
+        bank.addCustomerTransaction("Melville", "Caylem", 200.25);
+        bank.addCustomerTransaction("Melville", "Caylem", 50.5);
+        bank.addCustomerTransaction("Melville", "Caylem", 340.00);
+
+        bank.addCustomerTransaction("Melville", "Mike", 100.0);
+        bank.addCustomerTransaction("Melville", "James", 200.25);
+
+        bank.listCustomers("Melville", false);
+        bank.listCustomers("JHB", false);
+
+        bank.addBranch("Roodepoort");
+
+        if(!bank.addCustomer("Roodepoort","Brian", 30.00)) {
+            System.out.println("\nError, Roodepoort branch does not exist!");
+        }
+
+        if(!bank.addBranch("JHB")) {
+            System.out.println("JHB Branch already exists!");
+        }
+    }
+}
